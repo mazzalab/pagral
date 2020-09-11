b62abba20b72276b9f2f9a3742b3b7231cf9ec32
t.mazza <t.mazza@css-mendel.it>
2020-09-11 15:42:27 +0200

    [build] passing build version to setup.py

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-$PYTHON setup.py install --single-version-externally-managed --record=$RECIPE_DIR/record.txt
+$PYTHON setup.py install --single-version-externally-managed --record=$RECIPE_DIR/record.txt --version "%APPVEYOR_BUILD_VERSION%"
