3128b8323752fa3ead7644ebe291c0555e7a8e0d
t.mazza <t.mazza@css-mendel.it>
2020-09-11 20:28:44 +0200

    [build] passing build version to setup.py

--- setup.py
+++ setup.py
@@ -47,13 +47,15 @@ if USE_CYTHON:
 
 if 'APPVEYOR_BUILD_VERSION' not in os.environ:
     VERSION = os.environ['PKG_VERSION']
-
-print("\n\n"+", ".join(sys.argv)+"\n\n")
-if sys.argv[1] == "install":
-    VERSION = sys.argv[5]
-    del sys.argv[4:6]
 else:
     VERSION = os.environ['APPVEYOR_BUILD_VERSION']
+
+# print("\n\n"+", ".join(sys.argv)+"\n\n")
+# if sys.argv[1] == "install":
+#     VERSION = sys.argv[5]
+#     del sys.argv[4:6]
+# else:
+#     VERSION = os.environ['APPVEYOR_BUILD_VERSION']
 
 print("version {} passed to setup.py".format(VERSION))
 assert re.match('^[0-9]+\.[0-9]+\.[0-9]+$', VERSION), "Invalid version number"
