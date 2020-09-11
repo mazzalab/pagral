2ac488bd06cb7cf55a86fc3adf12b5470394b74e
t.mazza <t.mazza@css-mendel.it>
2020-09-11 19:49:16 +0200

    [build] passing build version to setup.py

--- setup.py
+++ setup.py
@@ -45,8 +45,9 @@ if USE_CYTHON:
                                  annotate=True)
 
 
-if not os.environ['APPVEYOR_BUILD_VERSION']:
+if 'APPVEYOR_BUILD_VERSION' not in os.environ:
     print(os.environ)
+    print("BUILD VER NOT FOUND!!!!")
 
 print("\n\n"+", ".join(sys.argv)+"\n\n")
 if sys.argv[1] == "install":
